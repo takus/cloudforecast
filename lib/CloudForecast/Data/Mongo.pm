a7e93f67537c97102ef4677497480cb7b412ebfa
Takumi Sakamoto <takumi.saka@gmail.com>
2014-05-25 23:26:20 +0900

    Fixed format

--- lib/CloudForecast/Data/Mongo.pm
+++ lib/CloudForecast/Data/Mongo.pm
@@ -61,6 +61,18 @@ fetcher {
 
     my $stats = decode_json $response->content;
 
+    my @sysinfo;
+    if ( $stats->{serverStatus}->{version} ) {
+        push @sysinfo, 'version' => $stats->{serverStatus}->{version};
+    }
+    if ( my $uptime = $stats->{serverStatus}->{uptime} ) {
+        my $day  = int( $uptime /86400 );
+        my $hour = int( ( $uptime % 86400 ) / 3600 );
+        my $min  = int( ( ( $uptime % 86400 ) % 3600) / 60 );
+        push @sysinfo, 'uptime' =>  sprintf("up %d days, %2d:%02d", $day, $hour, $min);
+    }
+    $c->ledge_set( 'sysinfo', \@sysinfo );
+
     return [
         ( map { int($stats->{serverStatus}->{backgroundFlushing}->{$_}) }
             qw/ average_ms last_ms flushes total_ms / ),
@@ -81,21 +93,21 @@ DEF:my2=<%RRD%>:back_total_ms:AVERAGE
 DEF:my3=<%RRD%>:back_average_ms:AVERAGE
 DEF:my4=<%RRD%>:back_last_ms:AVERAGE
 LINE1:my1#71FF06:Back Flushes   
-GPRINT:my1:LAST:Cur\:%6.2lf
-GPRINT:my1:AVERAGE:Ave\:%6.2lf
-GPRINT:my1:MAX:Max\:%6.2lf\l
+GPRINT:my1:LAST:Cur\:%6.2lf%s
+GPRINT:my1:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my1:MAX:Max\:%6.2lf%s\l
 LINE1:my2#FF2400:Back Total MS  
-GPRINT:my2:LAST:Cur\:%6.2lf
-GPRINT:my2:AVERAGE:Ave\:%6.2lf
-GPRINT:my2:MAX:Max\:%6.2lf\l
+GPRINT:my2:LAST:Cur\:%6.2lf%s
+GPRINT:my2:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my2:MAX:Max\:%6.2lf%s\l
 LINE1:my3#E83089:Back Average MS
-GPRINT:my3:LAST:Cur\:%6.2lf
-GPRINT:my3:AVERAGE:Ave\:%6.2lf
-GPRINT:my3:MAX:Max\:%6.2lf\l
+GPRINT:my3:LAST:Cur\:%6.2lf%s
+GPRINT:my3:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my3:MAX:Max\:%6.2lf%s\l
 LINE1:my4#17D2E1:Back Last MS   
-GPRINT:my4:LAST:Cur\:%6.2lf
-GPRINT:my4:AVERAGE:Ave\:%6.2lf
-GPRINT:my4:MAX:Max\:%6.2lf\l
+GPRINT:my4:LAST:Cur\:%6.2lf%s
+GPRINT:my4:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my4:MAX:Max\:%6.2lf%s\l
 
 @@ cmd 
 DEF:my1=<%RRD%>:op_inserts:AVERAGE
@@ -105,29 +117,29 @@ DEF:my4=<%RRD%>:op_deletes:AVERAGE
 DEF:my5=<%RRD%>:op_getmores:AVERAGE
 DEF:my6=<%RRD%>:op_commands:AVERAGE
 LINE1:my1#FF7200:Op Inserts 
-GPRINT:my1:LAST:Cur\:%6.2lf
-GPRINT:my1:AVERAGE:Ave\:%6.2lf
-GPRINT:my1:MAX:Max\:%6.2lf\l
+GPRINT:my1:LAST:Cur\:%6.2lf%s
+GPRINT:my1:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my1:MAX:Max\:%6.2lf%s\l
 LINE1:my2#503001:Op Queries 
-GPRINT:my2:LAST:Cur\:%6.2lf
-GPRINT:my2:AVERAGE:Ave\:%6.2lf
-GPRINT:my2:MAX:Max\:%6.2lf\l
+GPRINT:my2:LAST:Cur\:%6.2lf%s
+GPRINT:my2:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my2:MAX:Max\:%6.2lf%s\l
 LINE1:my3#EDAC00:Op Updates 
-GPRINT:my3:LAST:Cur\:%6.2lf
-GPRINT:my3:AVERAGE:Ave\:%6.2lf
-GPRINT:my3:MAX:Max\:%6.2lf\l
+GPRINT:my3:LAST:Cur\:%6.2lf%s
+GPRINT:my3:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my3:MAX:Max\:%6.2lf%s\l
 LINE1:my4#506101:Op Deletes 
-GPRINT:my4:LAST:Cur\:%6.2lf
-GPRINT:my4:AVERAGE:Ave\:%6.2lf
-GPRINT:my4:MAX:Max\:%6.2lf\l
+GPRINT:my4:LAST:Cur\:%6.2lf%s
+GPRINT:my4:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my4:MAX:Max\:%6.2lf%s\l
 LINE1:my5#0CCCCC:Op Getmores
-GPRINT:my5:LAST:Cur\:%6.2lf
-GPRINT:my5:AVERAGE:Ave\:%6.2lf
-GPRINT:my5:MAX:Max\:%6.2lf\l
+GPRINT:my5:LAST:Cur\:%6.2lf%s
+GPRINT:my5:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my5:MAX:Max\:%6.2lf%s\l
 LINE1:my6#53CA05:Op Commands
-GPRINT:my6:LAST:Cur\:%6.2lf
-GPRINT:my6:AVERAGE:Ave\:%6.2lf
-GPRINT:my6:MAX:Max\:%6.2lf\l
+GPRINT:my6:LAST:Cur\:%6.2lf%s
+GPRINT:my6:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my6:MAX:Max\:%6.2lf%s\l
 
 @@ conn 
 DEF:my1=<%RRD%>:connected_clients:AVERAGE
@@ -142,35 +154,35 @@ DEF:my2=<%RRD%>:index_hits:AVERAGE
 DEF:my3=<%RRD%>:index_misses:AVERAGE
 DEF:my4=<%RRD%>:index_resets:AVERAGE
 LINE1:my1#FF7200:Index Accesses
-GPRINT:my1:LAST:Cur\:%6.2lf
-GPRINT:my1:AVERAGE:Ave\:%6.2lf
-GPRINT:my1:MAX:Max\:%6.2lf\l
+GPRINT:my1:LAST:Cur\:%6.2lf%s
+GPRINT:my1:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my1:MAX:Max\:%6.2lf%s\l
 LINE1:my2#503001:Index Hits    
-GPRINT:my2:LAST:Cur\:%6.2lf
-GPRINT:my2:AVERAGE:Ave\:%6.2lf
-GPRINT:my2:MAX:Max\:%6.2lf\l
+GPRINT:my2:LAST:Cur\:%6.2lf%s
+GPRINT:my2:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my2:MAX:Max\:%6.2lf%s\l
 LINE1:my3#EDAC00:Index Misses  
-GPRINT:my3:LAST:Cur\:%6.2lf
-GPRINT:my3:AVERAGE:Ave\:%6.2lf
-GPRINT:my3:MAX:Max\:%6.2lf\l
+GPRINT:my3:LAST:Cur\:%6.2lf%s
+GPRINT:my3:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my3:MAX:Max\:%6.2lf%s\l
 LINE1:my4#506101:Index Resets  
-GPRINT:my4:LAST:Cur\:%6.2lf
-GPRINT:my4:AVERAGE:Ave\:%6.2lf
-GPRINT:my4:MAX:Max\:%6.2lf\l
+GPRINT:my4:LAST:Cur\:%6.2lf%s
+GPRINT:my4:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my4:MAX:Max\:%6.2lf%s\l
 
 @@ memory
 DEF:my1=<%RRD%>:used_mapped_mem:AVERAGE
 DEF:my2=<%RRD%>:used_virtual_mem:AVERAGE
 DEF:my3=<%RRD%>:used_resident_mem:AVERAGE
 AREA:my1#6FD1BF:Used Virtual Memory 
-GPRINT:my1:LAST:Cur\:%6.2lf
-GPRINT:my1:AVERAGE:Ave\:%6.2lf
-GPRINT:my1:MAX:Max\:%6.2lf\l
+GPRINT:my1:LAST:Cur\:%6.2lf%s
+GPRINT:my1:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my1:MAX:Max\:%6.2lf%s\l
 AREA:my2#6FD1BF:Used Mapped Memory  
-GPRINT:my2:LAST:Cur\:%6.2lf
-GPRINT:my2:AVERAGE:Ave\:%6.2lf
-GPRINT:my2:MAX:Max\:%6.2lf\l
+GPRINT:my2:LAST:Cur\:%6.2lf%s
+GPRINT:my2:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my2:MAX:Max\:%6.2lf%s\l
 AREA:my3#0E6E5C:Used Resident Memory
-GPRINT:my3:LAST:Cur\:%6.2lf
-GPRINT:my3:AVERAGE:Ave\:%6.2lf
-GPRINT:my3:MAX:Max\:%6.2lf\l
+GPRINT:my3:LAST:Cur\:%6.2lf%s
+GPRINT:my3:AVERAGE:Ave\:%6.2lf%s
+GPRINT:my3:MAX:Max\:%6.2lf%s\l
